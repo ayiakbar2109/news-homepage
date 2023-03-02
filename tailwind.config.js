4465a5ce33ba99cc6a66b4213e5f568d5c5fd0f5
Ayi Akbar Maulana <ayiakbar66@gmail.com>
2023-03-02 21:15:26 +0700

    menambahkan media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "off-white": "var(--off-white)",
+        "very-dark-blue": "var(--very-dark-blue)",
+        "grayish-blue": "var(--grayish-blue)",
+        "dark-grayish-blue": "var(--dark-grayish-blue)",
+        "soft-orange": "var(--soft-orange)",
+        "soft-red": "var(--soft-red)"
+      }
+    },
   },
   plugins: [],
 }
